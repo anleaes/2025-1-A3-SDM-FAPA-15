1a1d37d689828b5d7d543d3996a490bdadd5ee47
AlexandreBassani <xandi.abb@gmail.com>
2025-06-22 15:49:31 -0300

    feat(stage): create Stage model with relation to Project

--- obraapp/apps/stage/models.py
+++ obraapp/apps/stage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from project.models import Project
 
-# Create your models here.
+class Stage(models.Model):
+    name = models.CharField("Name", max_length=100)
+    description = models.TextField("Description", blank=True)
+    order = models.IntegerField("Order")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="stages")
+
+    class Meta:
+        verbose_name = "Stage"
+        verbose_name_plural = "Stages"
+        ordering = ["order"]
+
+    def __str__(self):
+        return f"{self.name} - {self.project.name}"
