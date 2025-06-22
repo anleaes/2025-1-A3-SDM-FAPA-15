c519f9da24252437174663ee2fd2c2cd8b0ec08e
AlexandreBassani <xandi.abb@gmail.com>
2025-06-22 17:54:26 -0300

    feat(employeestage): create EmployeeStage model as M2M relationship

--- obraapp/apps/employeestage/models.py
+++ obraapp/apps/employeestage/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from employee.models import Employee
+from stage.models import Stage
 
-# Create your models here.
+class EmployeeStage(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='stages')
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE, related_name='employees')
+
+    class Meta:
+        verbose_name = "Employee Assignment"
+        verbose_name_plural = "Employee Assignments"
+        unique_together = ('employee', 'stage')
+        ordering = ['id']
+
+    def __str__(self):
+        return f"{self.employee.name} in {self.stage.name}"
+
