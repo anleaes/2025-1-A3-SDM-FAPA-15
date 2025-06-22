5747f745572550acb1adf20976b48dff0b65bd48
AlexandreBassani <xandi.abb@gmail.com>
2025-06-22 17:40:54 -0300

    feat(employee): create Employee model with personal data

--- obraapp/apps/employee/models.py
+++ obraapp/apps/employee/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField("Name", max_length=100)
+    position = models.CharField("Position", max_length=100)
+    phone = models.CharField("Phone", max_length=20)
+    email = models.EmailField("Email")
+
+    class Meta:
+        verbose_name = "Employee"
+        verbose_name_plural = "Employees"
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.name
