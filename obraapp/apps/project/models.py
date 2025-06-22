4c4410806b356cf19883d70a4af0bbde0e458a0c
AlexandreBassani <xandi.abb@gmail.com>
2025-06-22 14:16:16 -0300

    feat(project): create Project model

--- obraapp/apps/project/models.py
+++ obraapp/apps/project/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from projecttype.models import ProjectType
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField("Name", max_length=100)
+    description = models.TextField("Description", blank=True)
+    start_date = models.DateField("Start Date")
+    in_progress = models.BooleanField("In Progress", default=True)
+    project_type = models.ForeignKey(ProjectType, on_delete=models.CASCADE, related_name="projects")
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.name
