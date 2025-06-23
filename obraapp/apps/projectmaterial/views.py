e5bbeecf95c086bd732ea88101af0e1728b4e535
AlexandreBassani <xandi.abb@gmail.com>
2025-06-23 17:11:20 -0300

    feat(projectmaterial): create viewset for ProjectMaterial API

--- obraapp/apps/projectmaterial/views.py
+++ obraapp/apps/projectmaterial/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ProjectMaterial
+from .serializer import ProjectMaterialSerializer
 
-# Create your views here.
+class ProjectMaterialViewSet(viewsets.ModelViewSet):
+    queryset = ProjectMaterial.objects.all()
+    serializer_class = ProjectMaterialSerializer
